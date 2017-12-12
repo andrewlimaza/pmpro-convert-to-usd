bf807cd51e07ccfc7c311f7f780bcc52fe354cca
Andrew Lima <andrewlima@Andrews-MacBook-Pro.local>
2017-12-12 14:14:31 +0200

    Admin updates
    Added in updates for admins to include a currency change when a user
    saves payment settings.

--- pmpro-convert-to-usd.php
+++ pmpro-convert-to-usd.php
@@ -16,10 +16,28 @@
 defined( 'ABSPATH' ) or exit;
 
 /**
+ * Allow user's to delete the transient for the conversion rate.
+ * @since 1.0
+ */
+function pcc_delete_transient() {
+	
+	if( !empty( $_REQUEST['pmpro_clear_transient'] ) ){
+		global $pmpro_msg, $pmpro_msgt;
+
+		delete_transient( 'pmpro_usd_value' );
+
+		$pmpro_msg = __( 'Transient cleared for currency convertor', 'pmpro-currency-converter' );
+		$pmpro_msgt = 'pmpro_error';
+	}
+
+}
+add_action( 'init', 'pcc_delete_transient' );
+
+/**
  * Change currency to USD before checking out.
  * @since 1.0
  */
-function pmpro_change_to_usd_before_checkout(){
+function pcc_change_to_usd_before_checkout(){
 	global $pmpro_level, $pmpro_currency;
 
 	// Bail if already in USD
@@ -30,23 +48,26 @@ function pmpro_change_to_usd_before_checkout(){
 	// Set currency to USD
 	$pmpro_currency = 'USD';
 
+	// Should maybe add in a filter to allow developers to expand on this code???
+	//do_action( 'ppc_before_checkout' );
+
 	if( $pmpro_level->initial_payment !== '0.00' ){
-		$pmpro_level->initial_payment = pmpro_convert_amount_usd( $pmpro_level->initial_payment );
+		$pmpro_level->initial_payment = pcc_convert_amount_usd( $pmpro_level->initial_payment );
 	}
 
 	if( $pmpro_level->billing_amount !== '0.00' ){
-		$pmpro_level->billing_amount = pmpro_convert_amount_usd( $pmpro_level->billing_amount );
+		$pmpro_level->billing_amount = pcc_convert_amount_usd( $pmpro_level->billing_amount );
 	}
 	
 }
 
-add_action( 'pmpro_checkout_before_processing', 'pmpro_change_to_usd_before_checkout' );
+add_action( 'pmpro_checkout_before_processing', 'pcc_change_to_usd_before_checkout' );
 
 /**
  * Change level cost text to show USD values next to locale values.
  * @since 1.0
  */
-function pmpro_show_usd_cost_text_value( $cost, $level ){
+function pcc_show_usd_cost_text_value( $cost, $level ){
 
 	global $pmpro_currency;
 
@@ -59,12 +80,12 @@ function pmpro_show_usd_cost_text_value( $cost, $level ){
 	$billing_amount = $level->billing_amount;
 
 	if( $initial_payment !== '0.00' ){
-		$initial_payment_usd = round( pmpro_convert_amount_usd( $initial_payment ), 2 );
+		$initial_payment_usd = round( pcc_convert_amount_usd( $initial_payment ), 2 );
 		$cost = str_replace( $initial_payment, $initial_payment . ' ( $' . $initial_payment_usd . ' approx. ) ', $cost );
 	}
 
 	if( $billing_amount !== '0.00' ){
-		$billing_payment_usd = round( pmpro_convert_amount_usd( $billing_amount ), 2 );
+		$billing_payment_usd = round( pcc_convert_amount_usd( $billing_amount ), 2 );
 		$cost = str_replace( $billing_amount, $billing_amount . ' ( $' . $billing_payment_usd . ' approx. ) ', $cost );
 	}
 
@@ -72,18 +93,18 @@ function pmpro_show_usd_cost_text_value( $cost, $level ){
 	return $cost;
 }
 
-add_filter( 'pmpro_level_cost_text', 'pmpro_show_usd_cost_text_value', 20, 2 );
+add_filter( 'pmpro_level_cost_text', 'pcc_show_usd_cost_text_value', 20, 2 );
 
 /**
  * Function that converts the amount to USD
  * @uses https://api.fixer.io
  * @since 1.0
  */
-function pmpro_convert_amount_usd( $amount ){
+function pcc_convert_amount_usd( $amount ){
 
 	$amount = floatval( $amount );
 
-	delete_transient('pmpro_usd_value');
+	//delete_transient('pmpro_usd_value');
 
 	if( false === ( $conversion_rate = get_transient( 'pmpro_usd_value' ) ) ){
 
@@ -110,3 +131,38 @@ function pmpro_convert_amount_usd( $amount ){
 
 	return $amount * $conversion_rate;
 }
+
+
+function pcc_admin_init(){
+
+	if( $_REQUEST['page'] === 'pmpro-paymentsettings' ){
+
+		//if we save the page, let's delete the transient to be safe.
+		if( !empty( $_REQUEST['savesettings'] ) ){
+			delete_transient( 'pmpro_usd_value' );	
+		}
+	}
+}
+add_action( 'admin_init', 'pcc_admin_init' );
+
+function pcc_admin_js_display_conversion() {
+
+	if( $_REQUEST['page'] === 'pmpro-paymentsettings' ){
+		$currency = pmpro_getOption( 'currency' );
+
+		$amount = pcc_convert_amount_usd( 1 );
+		?>
+		<script type="text/javascript">
+			jQuery(document).ready(function(){
+
+				var output = "<tr><td><small><strong><?php _e( 'Conversion Rate', 'pmpro-convert-to-usd' ); ?>: </strong><?php echo '1 USD = ' . round( 1 / $amount, 2 ) . ' ' . $currency; ?></small></td></tr>";
+
+				jQuery('[name="currency"]').next().after(output);
+
+			});
+		</script>
+		<?php
+	}
+}
+
+add_action( 'admin_footer', 'pcc_admin_js_display_conversion' );
